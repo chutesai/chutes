4199f39b69086b2a37f0ccafbecab2ce13e5f9ac
namoray <neuvaamor@proton.me>
2024-11-10 14:32:42 +0100

    names

--- bin/chutes.py
+++ bin/chutes.py
@@ -18,9 +18,11 @@ app.command(name="register", help="Create an account with the chutes run platfor
 app.command(
     help="Create an API key for the chutes run platform!", no_args_is_help=True
 )(create_api_key)
-app.command(help="Report an invocation!", no_args_is_help=True)(report_invocation)
-app.command(help="Run a chute!", no_args_is_help=True)(run_chute)
-app.command(help="Deploy a chute!", no_args_is_help=True)(deploy_chute)
-app.command(help="Build an image!", no_args_is_help=True)(build_image)
+app.command(help="Report an invocation!", no_args_is_help=True, name="report")(
+    report_invocation
+)
+app.command(help="Run a chute!", no_args_is_help=True, name="run")(run_chute)
+app.command(help="Deploy a chute!", no_args_is_help=True, name="deploy")(deploy_chute)
+app.command(help="Build an image!", no_args_is_help=True, name="build")(build_image)
 if __name__ == "__main__":
     app()
