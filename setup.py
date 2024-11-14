35d98dccc2b6f170c249b372476e3778fb90f8cd
Jon Durbin <jon@jondurbin.com>
2024-11-14 10:44:25 -0500

    Fix CLI/bin.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"))
 
 setup(
     name="chutes",
-    version="0.0.20",
+    version="0.0.21",
     description="Chutes development kit and CLI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -46,7 +46,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "chutes=bin.chutes:app",
+            "chutes=chutes.cli:app",
         ],
     },
 )
