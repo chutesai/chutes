3b20099aecfc983086f61be4e95a4a5e6c2dd57b
Jon Durbin <jon@jondurbin.com>
2024-11-20 10:28:52 -0500

    GraVal middleware (#4)

--- chutes/image/standard/vllm.py
+++ chutes/image/standard/vllm.py
@@ -1,4 +1,4 @@
-VLLM = "parachutes/vllm:0.6.3"
+VLLM = "parachutes/vllm:0.6.2"
 
 # To build this yourself, you can use something like:
 # from chutes.image import Image  # noqa: E402
@@ -6,15 +6,15 @@ VLLM = "parachutes/vllm:0.6.3"
 # image = (
 #     Image("vllm-custom", "0.6.2")
 #     .with_python("3.12.7")
-#     .apt_install(["google-perftools", "git"])
+#     .apt_install(["git"])
 #     .run_command("useradd vllm -s /sbin/nologin")
 #     .run_command(
-#         "mkdir -p /workspace /home/vllm && chown vllm:vllm /workspace /home/vllm"
+#         "mkdir -p /app /home/vllm && chown vllm:vllm /app /home/vllm"
 #     )
 #     .set_user("vllm")
-#     .set_workdir("/workspace")
+#     .set_workdir("/app")
 #     .with_env("PATH", "/opt/python/bin:$PATH")
-#     .run_command("/opt/python/bin/pip install --no-cache vllm==0.6.2 wheel packaging")
-#     .run_command("/opt/python/bin/pip install --no-cache flash-attn==2.6.3")
+#     .run_command("/opt/python/bin/pip install --no-cache 'vllm<0.6.4' wheel packaging")
+#     .run_command("/opt/python/bin/pip install --no-cache flash-attn")
 #     .run_command("/opt/python/bin/pip uninstall -y xformers")
 # )
