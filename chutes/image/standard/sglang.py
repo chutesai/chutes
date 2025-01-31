5eb5fe2271a535874a8ea5474417837a4a952d5f
Jon Durbin <jon@jondurbin.com>
2025-01-31 07:00:36 -0500

    Ensure response is defined, image template updates.

--- chutes/image/standard/sglang.py
+++ chutes/image/standard/sglang.py
@@ -1,10 +1,15 @@
-SGLANG = "chutes/sglang:0.4.2.post0"
+SGLANG = "chutes/sglang:0.4.2.post01"
 
 # To build this yourself, you can use something like:
-# from chutes.image import Image  # noqa: E402
+# from chutes.image import Image
 # image = (
-#     Image(username="chutes", name="sglang", tag="0.4.1.post6", readme="SGLang is a fast serving framework for large language models and vision language models. It makes your interaction with models faster and more controllable by co-designing the backend runtime and frontend language.")
+#     Image(
+#         username="chutes",
+#         name="sglang",
+#         tag="0.4.2.post01",
+#         readme="SGLang is a fast serving framework for large language models and vision language models. It makes your interaction with models faster and more controllable by co-designing the backend runtime and frontend language."
+#     )
 #     .from_base("parachutes/base-python:3.12.7")
 #     .run_command("pip install --upgrade pip")
-#     .run_command("pip install "sglang[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/")
+#     .run_command("pip install 'sglang[all]==0.4.2' --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/")
 # )
--- chutes/image/standard/vllm.py
+++ chutes/image/standard/vllm.py
@@ -1,11 +1,11 @@
 VLLM = "chutes/vllm:0.7.0"
 
 # To build this yourself, you can use something like:
-# from chutes.image import Image  # noqa: E402
 # image = (
-#     Image(username="chutes", name="vllm", tag="0.6.4", readme="## vLLM - fast, flexible llm inference")
+#     Image(
+#         username="chutes", name="vllm", tag="0.7.0", readme="## vLLM - fast, flexible llm inference"
+#     )
 #     .from_base("parachutes/base-python:3.12.7")
-#     .run_command("pip install --no-cache 'vllm<0.6.5' wheel packaging")
+#     .run_command("pip install --no-cache 'vllm==0.7.0' wheel packaging git+https://github.com/huggingface/transformers.git@ec7afad60909dd97d998c1f14681812d69a15728 qwen-vl-utils[decord]==0.0.8")
 #     .run_command("pip install --no-cache flash-attn")
-#     .run_command("pip uninstall -y xformers")
 # )
