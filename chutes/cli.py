61c4fb3bc2e2a7f59777d67455f4735b14f42b02
Jon Durbin <jon@jondurbin.com>
2025-02-06 07:47:07 -0500

    Change fingerprint via hotkey signatures.

--- chutes/cli.py
+++ chutes/cli.py
@@ -8,6 +8,7 @@ from chutes.entrypoint.build import build_image
 from chutes.entrypoint.report import report_invocation
 from chutes.entrypoint.run import run_chute
 from chutes.entrypoint.link import link_hotkey
+from chutes.entrypoint.fingerprint import change_fingerprint
 from chutes.crud import chutes_app, images_app, api_keys_app
 
 app = typer.Typer(no_args_is_help=True)
@@ -18,6 +19,9 @@ app.command(
     name="link",
     help="Link a validator or subnet owner hotkey to your account, which grants free+developer access!",
 )(link_hotkey)
+app.command(help="Change your fingerprint!", no_args_is_help=True, name="refinger")(
+    change_fingerprint
+)
 app.command(help="Report an invocation!", no_args_is_help=True, name="report")(report_invocation)
 app.command(help="Run a chute!", no_args_is_help=True, name="run")(run_chute)
 app.command(help="Deploy a chute!", no_args_is_help=True, name="deploy")(deploy_chute)
