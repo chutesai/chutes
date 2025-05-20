d54482440ed7614778534bf97826e77cde301dda
Jon Durbin <jon@jondurbin.com>
2025-05-20 03:35:19 -0400

    Tokenization endpoints for sglang.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"))
 
 setup(
     name="chutes",
-    version="0.3.0",
+    version="0.2.42",
     description="Chutes development kit and CLI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -37,7 +37,7 @@ setup(
         "substrate-interface>=1.7.11",
         "rich>=13.0.0",
         "typer>=0.12.5",
-        "graval>=0.1.1",
+        "graval==0.0.6",
         "prometheus-client==0.21.0",
         "cryptography",
         "psutil",
