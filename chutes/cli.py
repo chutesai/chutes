c4285c541b86f56829a7717fb972cf6b1636b221
Jon Durbin <jon@jondurbin.com>
2025-09-10 12:08:46 +0000

    Warmup entrypoint.

--- chutes/cli.py
+++ chutes/cli.py
@@ -14,6 +14,7 @@ from chutes.entrypoint.report import report_invocation
 from chutes.entrypoint.run import run_chute
 from chutes.entrypoint.fingerprint import change_fingerprint
 from chutes.entrypoint.share import share_chute
+from chutes.entrypoint.warmup import warmup_chute
 from chutes.crud import chutes_app, images_app, api_keys_app
 
 app = typer.Typer(no_args_is_help=True)
@@ -39,6 +40,7 @@ app.command(help="Run a chute!", no_args_is_help=True, name="run")(run_chute)
 app.command(help="Deploy a chute!", no_args_is_help=True, name="deploy")(deploy_chute)
 app.command(help="Build an image!", no_args_is_help=True, name="build")(build_image)
 app.command(help="Share a chute!", no_args_is_help=True, name="share")(share_chute)
+app.command(help="Warm up a chute!", no_args_is_help=True, name="warmup")(warmup_chute)
 
 # Chutes
 app.add_typer(chutes_app, name="chutes")
