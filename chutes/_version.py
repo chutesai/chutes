9bc9c4c2d02cc7d0f87815b32c09d64e32119195
Jon Durbin <jon@jondurbin.com>
2025-10-18 14:50:56 +0000

    NN udpate

--- chutes/_version.py
+++ chutes/_version.py
@@ -1,1 +1,1 @@
-version = "0.3.39.rc9"
+version = "0.3.40.rc1"
--- chutes/entrypoint/run.py
+++ chutes/entrypoint/run.py
@@ -21,6 +21,7 @@ import subprocess
 import threading
 import traceback
 import orjson as json
+from functools import lru_cache
 from loguru import logger
 from typing import Optional, Any
 from datetime import datetime
@@ -42,6 +43,22 @@ from cryptography.hazmat.primitives import padding
 
 
 CFSV_PATH = os.path.join(os.path.dirname(__file__), "..", "cfsv")
+
+
+@lru_cache(maxsize=1)
+def get_netnanny_ref():
+    netnanny = ctypes.CDLL(None, ctypes.RTLD_GLOBAL)
+    netnanny.generate_challenge_response.argtypes = [ctypes.c_char_p]
+    netnanny.generate_challenge_response.restype = ctypes.c_char_p
+    netnanny.verify_challenge.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+    netnanny.verify_challenge.restype = ctypes.c_int
+    netnanny.initialize_network_control.argtypes = []
+    netnanny.initialize_network_control.restype = ctypes.c_int
+    netnanny.unlock_network.argtypes = []
+    netnanny.unlock_network.restype = ctypes.c_int
+    netnanny.lock_network.argtypes = []
+    netnanny.lock_network.restype = ctypes.c_int
+    return netnanny
 
 
 def get_all_process_info():
@@ -126,6 +143,18 @@ async def process_fs_challenge(request: Request):
             offset=challenge.offset,
             length=challenge.length,
         ),
+        media_type="text/plain",
+    )
+
+
+async def process_netnanny_challenge(request: Request):
+    """
+    Process a NetNanny challenge.
+    """
+    challenge = request.state.decrypted.get("challenge", "foo")
+    netnanny = get_netnanny_ref()
+    return Response(
+        content=netnanny.generate_challenge_response(challenge),
         media_type="text/plain",
     )
 
@@ -719,17 +748,7 @@ def run_chute(
 
         # Configure net-nanny.
         try:
-            netnanny = ctypes.CDLL(None, ctypes.RTLD_GLOBAL)
-            netnanny.generate_challenge_response.argtypes = [ctypes.c_char_p]
-            netnanny.generate_challenge_response.restype = ctypes.c_char_p
-            netnanny.verify_challenge.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-            netnanny.verify_challenge.restype = ctypes.c_int
-            netnanny.initialize_network_control.argtypes = []
-            netnanny.initialize_network_control.restype = ctypes.c_int
-            netnanny.unlock_network.argtypes = []
-            netnanny.unlock_network.restype = ctypes.c_int
-            netnanny.lock_network.argtypes = []
-            netnanny.lock_network.restype = ctypes.c_int
+            netnanny = get_netnanny_ref()
             challenge = secrets.token_hex(16).encode("utf-8")
             response = netnanny.generate_challenge_response(challenge)
             if not response:
@@ -909,6 +928,7 @@ def run_chute(
         chute.add_api_route("/_device_challenge", process_device_challenge, methods=["GET"])
         chute.add_api_route("/_fs_challenge", process_fs_challenge, methods=["POST"])
         chute.add_api_route("/_fs_hash", _handle_fs_hash_challenge, methods=["POST"])
+        chute.add_api_route("/_netnanny_challenge", process_netnanny_challenge, methods=["POST"])
 
         # New envdump endpoints.
         import chutes.envdump as envdump
